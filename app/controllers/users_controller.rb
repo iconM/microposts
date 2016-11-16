6b617928acf23f91b9890a2c8544cfff31b2a883
mariko okuizumi <mariko.shimazu@gmail.com>
2016-11-16 08:40:17 +0000

    profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
+    params.require(:user).permit(:name, :email, :introduction, :area, :password,
                                  :password_confirmation)
   end
 end
