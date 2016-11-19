b0283939db9397ecded320a11785da271b40c738
Mariko O <mari25.van@gmail.com>
2016-11-18 17:42:26 -0800

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update, :show]
+  
   def show # 追加
    @user = User.find(params[:id])
   end
@@ -38,4 +40,13 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :introduction, :area, :password,
                                  :password_confirmation)
   end
+      # beforeフィルター
+
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
