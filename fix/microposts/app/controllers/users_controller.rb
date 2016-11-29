835845e449d13f82d693cecfae6f9dc9a10f4cac
mariko okuizumi <mariko.shimazu@gmail.com>
2016-11-29 01:58:56 +0000

    add home view's timeline

--- fix/microposts/app/controllers/users_controller.rb
+++ fix/microposts/app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   end
   
   def create
+  # binding.pry
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
@@ -53,7 +54,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
+    params.require(:user).permit(:name, :email, :password, :introduction, :area ,
                                  :password_confirmation)
   end
   
